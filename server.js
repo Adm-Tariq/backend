f3f6a44f3ef74e0b3f042d8f77c5ede2ebf6d2af
Adm-Tariq <adm.tariq11@gmail.com>
2024-07-24 11:10:36 +0300

    backenEUD

---
+++ server.js
@@ -0,0 +1,35 @@
+import mongoose, { Mongoose } from "mongoose";
+import express from 'express'
+import cors from 'cors'
+import contraction from "./config/contraction.js";
+
+import coursesRoute from './routers/coursesRoute.js'
+
+
+const app = express()
+const port = 3000
+contraction()
+app.use(express.json())
+
+const corsOptions = {
+    origin: 'http://localhost:3001',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
+
+
+
+
+app.use('/api/courses', coursesRoute)
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+mongoose.connection.once('open', () => {
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    })
+    console.log('Connected DB');
+})
